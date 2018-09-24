daa003c5de944db4b06b98d640596e1175a44374
motek <moteko.0608.mrchildren@gmail.com>
2018-09-24 11:46:11 +0900

    initial commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void fizzbuzz() {
+        assertEquals("fizzbuzz", Main.fizzbuzz(15, ""));
+    }
+    @Test
+    public void fizz() {
+        assertEquals("fizz", Main.fizzbuzz(3, ""));
+    }
+    @Test
+    public void buzz() {
+        assertEquals("buzz", Main.fizzbuzz(5, ""));
+    }
+    @Test
+    public void number() {
+        assertEquals("7", Main.fizzbuzz(7, ""));
+    }
+
+}
